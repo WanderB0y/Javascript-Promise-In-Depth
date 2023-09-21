87d94072e1794c7d4751355469b2dbcd799ff5af
Rovic Villaralvo <102729133+kazewanderboy@users.noreply.github.com>
2023-09-21 08:34:59 +0800

    Done

--- newPromise.js
+++ newPromise.js
@@ -1,1 +1,27 @@
-const myPromise = new Promise((resolve, reject) => {})
+// const myPromise = new Promise((resolve, reject) => {
+//     setTimeout(()=> {
+//         console.log("Hello my friend B")
+//     }, 500)
+// })
+
+// console.log("Hello?");
+
+// const myPromises = new Promise((resolve, reject) => {
+//     setTimeout(()=> {
+//         console.log("Hello my friend A")
+//     }, 1500)
+// })
+
+let i = 0;
+    const myPromise = new Promise((resolve, reject) => {
+        setTimeout(()=> {
+           i += 40;
+        }, -1)
+    })
+
+    const forFuture = new Promise((resolve, reject)=> {
+        setTimeout(()=> {
+            console.log(i)
+        }, 2)
+    })
+console.log(i + "Nena")
